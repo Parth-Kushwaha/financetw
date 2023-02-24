cb5ee6018af835f830b2a4c4f4ff19a6472be445
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-25 01:01:16 +0530

    assets folder

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Laptop from '../assets/laptop.jpg'
 
 const Analytics = () => {
   return (
-    <div>
-      Analytics
+    <div className='w-full bg-white py-16 px-4'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+
+      </div>
     </div>
   )
 }
