526acdb163106be1ff3e5e9db31374d72512f50f
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-24 22:46:47 +0530

    nav start

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="text-white">
-      <h1>Bhai ka Finance</h1>
+    <div className="text-white flex justify-between items-center max-w-[1240px] mx-auto px-4">
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Bhai ka Finance</h1>
+      <ul className='flex'>
+        <li className='p-3'>HOME</li>
+        <li className='p-3'>COMPANY</li>
+        <li className='p-3'>RESOURCES</li>
+        <li className='p-3'>ABOUT</li>
+        <li className='p-3'>CONTACT</li>
+      </ul>
     </div>
   )
 }
