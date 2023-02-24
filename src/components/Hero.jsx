70f0a9a1b1141a48ff93baa85cee38973d479ed7
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-25 00:53:43 +0530

    hero done, analytics start

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Typed from 'react-typed'
+
+const Hero = () => {
+  return (
+    <div className='text-white '>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto flex text-center flex-col justify-center '>
+        <p className='text-[#00df9a]'>Grow with Data Analytics</p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold '>Grow with Data</h1>
+        <div className='flex justify-center items-center'>
+            <p className='md:text-4xl sm:text-5xl text-xl font-bold mt-1'>Fast, Flexible Financing for</p>
+            <Typed className='md:text-4xl sm:text-5xl text-xl font-bold mt-1 md:pl-4 pl-3 text-[#00df9a]'
+                strings={['BTB','B2C','SASS']} 
+                typeSpeed={120} 
+               backSpeed={140} loop/>
+        </div>
+        <p className='md:text-2xl sm:text-xl font-bold text-gray-400'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+        <button className='bg-[#00df9a] w-[150px] rounded-xl font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         
       </div>
-      <div className={!nav ? 'fixed left-0 top-0 w-[30%] h-full border-r border-r-gray-900 ease-in-out duration-400': 'fixed hidden'}>
+      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 ease-in-out duration-400': 'fixed hidden'}>
         {/* <h1 className='w-full text-3xl font-bold m-4 text-[#00df9a]'>Bhai ka Finance</h1> */}
         <ul className='p-4 mt-14'>
           <li className='p-3 border-b border-gray-500'>HOME</li>
