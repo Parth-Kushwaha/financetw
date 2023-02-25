3b5e9f0934a777393de54be2caeb70d8e0407999
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-25 22:26:37 +0530

    c

--- src/components/FooterLi.jsx
+++ src/components/FooterLi.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const FooterLi = ({list}) => {
   return (
-        <div>
-            <h6 className='font-medium'>{list.head}</h6>
-            <ul>
-                <li className='py-2 text-sm'>{list.li1}</li>
-                <li className='py-2 text-sm'>{list.li2}</li>
-                <li className='py-2 text-sm'>{list.li3}</li>
-                <li className='py-2 text-sm'>{list.li4}</li>
-                <li className='py-2 text-sm'>{list.li5}</li>
-            </ul>
-        </div>
+    <div>
+        <h6 className='font-medium'>{list.head}</h6>
+        <ul>
+            <li className='py-2 text-sm'>{list.li1}</li>
+            <li className='py-2 text-sm'>{list.li2}</li>
+            <li className='py-2 text-sm'>{list.li3}</li>
+            <li className='py-2 text-sm'>{list.li4}</li>
+            <li className='py-2 text-sm'>{list.li5}</li>
+        </ul>
+    </div>
   )
 }
 
