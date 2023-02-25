46cf1e1adab75c4dd3d29365cb30d9b25883bf6f
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-25 14:35:34 +0530

    bug fix in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,8 +21,8 @@ const Navbar = () => {
         {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         
       </div>
-      <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-gray-900 ease-in-out duration-500': 'fixed hidden'}>
-        <h1 className='w-full text-3xl font-bold m-4 text-[#00df9a]'>Bhai ka Finance</h1>
+      <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-gray-900 ease-in-out duration-500': 'fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold m-4 mt-8 text-[#00df9a]'>Bhai ka Finance</h1>
         <ul className='p-4'>
           <li className='p-3 border-b border-gray-500'>HOME</li>
           <li className='p-3 border-b border-gray-500'>COMPANY</li>
