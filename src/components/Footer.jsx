f7af9c0e98c0339b73c757331fc10c3a487b4fea
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-25 13:44:56 +0530

    footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,17 @@ FaTwitterSquare
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+        <div>
+            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Bhai ka Finance</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam labore, blanditiis illo obcaecati omnis ratione velit repellendus iste et. At placeat amet doloribus? Nemo, omnis! Ex velit esse rerum error!</p>
+            <div>
+                <FaFacebookSquare/>
+                <FaInstagramSquare/>
+                <FaGithubSquare/>
+                <FaTwitterSquare/>
+            </div>
+        </div>
     </div>
   )
 }
