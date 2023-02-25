3e8345d8692bfb83f650eeb4ed47f5c4f04c2ee3
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-25 14:29:52 +0530

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,12 +18,12 @@ const Navbar = () => {
         <li className='p-3'>CONTACT</li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+        {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         
       </div>
-      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 ease-in-out duration-400': 'fixed hidden'}>
-        {/* <h1 className='w-full text-3xl font-bold m-4 text-[#00df9a]'>Bhai ka Finance</h1> */}
-        <ul className='p-4 mt-14'>
+      <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-gray-900 ease-in-out duration-500': 'fixed hidden'}>
+        <h1 className='w-full text-3xl font-bold m-4 text-[#00df9a]'>Bhai ka Finance</h1>
+        <ul className='p-4'>
           <li className='p-3 border-b border-gray-500'>HOME</li>
           <li className='p-3 border-b border-gray-500'>COMPANY</li>
           <li className='p-3 border-b border-gray-500'>RESOURCES</li>
