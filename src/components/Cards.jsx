fa8fd4bc07ef62e299aa88a7bcd0d4aaac6b4d4f
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-25 12:18:38 +0530

    c

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,7 +8,9 @@ const Cards = () => {
     <div className='w-full py-[10rem] px-4 bg-white'>
         <div className='max-w-[1240px] mx-auto grid md:grid-col-3 gap-8'>
             <div className='w-full'>
-                <img src={Single} alt="/" />
+                <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
+                <h2>Single User</h2>
+                <p>$69</p>
             </div>
         </div>
       
